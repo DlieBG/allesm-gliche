9bd7c5a87956e1e94d4bcaa819ac787ce0ac285b
Benedikt <mail@benedikt-schwering.de>
2020-10-26 19:53:39 +0100

    Fehler behoben.

--- bikehunt/web/functions/getLocation.php
+++ bikehunt/web/functions/getLocation.php
@@ -11,7 +11,7 @@
             $query = mysqli_query($dbconnect, 'SELECT * FROM Standort LEFT JOIN Team ON Team.TeamNr = Standort.Team WHERE Team = '.$_SESSION['GegnerNr'].' ORDER BY StandortNr DESC LIMIT 1;');
             $location = mysqli_fetch_array($query);
 
-            print_r($location);
+            print_r(json_encode($location));
             die();
         }
     }
