afb3859a5d52b7b8b12373ae60aba5637a88e1e8
Benedikt <mail@benedikt-schwering.de>
2020-10-26 19:01:35 +0100

    SQL Query für Spezialablaufzeit.

--- bikehunt/web/functions/getLocation.php
+++ bikehunt/web/functions/getLocation.php
@@ -1,6 +1,8 @@
 <?php
     include('db.php');
 
+    //SELECT * FROM `Spezial` WHERE NOW() > Zeit AND NOW() - Zeit < 300;
+
     $query = mysqli_query($dbconnect, 'SELECT * FROM Standort LEFT JOIN Team ON Team.TeamNr = Standort.Team WHERE Standort.Zeit < NOW()-200 AND Team = '.$_SESSION['GegnerNr'].' ORDER BY StandortNr DESC LIMIT 1;');
     $location = mysqli_fetch_array($query);
 
